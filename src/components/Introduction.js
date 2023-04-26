b8ce2a33a47f36ee7a43b291247a06b945226a6b
Lehi Cano <onacihel@gmail.com>
2023-04-26 09:16:49 -0500

    adds animated secondary header on intro

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,5 +1,6 @@
 import { FaGithub, FaInstagram, FaLinkedin, FaYoutube, FaEnvelope} from "react-icons/fa"
 import { RiArrowDownCircleLine } from "react-icons/ri";
+import { MovingComponent } from 'react-moving-text'
 
 const Introduction = (props) => {
     return (
@@ -7,9 +8,15 @@ const Introduction = (props) => {
             <div className="mug">
             </div>    
             <div className="intro-type">
-                <h1 className="sm-h1">Lehi Cano</h1>
+                <h1>Hi, I'm Lehi.</h1>
                 <p className="pronounce">(I pronounce it Lay-hee)</p>
-                <h3 className="black">I'm a Full Stack Web Developer + Designer.</h3>
+                <h2 className="flex"> I'm a <MovingComponent type="typewriter" delay="0s" dataText={[
+                    "Web Developer",
+                    "User Interface Designer",
+                    "Bicycle Technologist",
+                    "Photographer",
+                ]}/>
+                </h2>
                 <div className="contact-icons">
                     <h2><a href="https://github.com/layhee" target="_blank" rel="noreferrer"><FaGithub/><span className="bars">Github</span></a></h2>
                     <h2><a href="https://www.linkedin.com/in/lehicano/" target="_blank" rel="noreferrer"><FaLinkedin/><span className="bars">Linked In</span></a></h2>
